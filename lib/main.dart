da9e26db4607b488a161c3b8aeccb41060c61d27
Sathvik-Malgikar <Sathvik.malgikar@gmail.com>
2024-02-09 01:10:24 +0530

    First div complete

--- lib/main.dart
+++ lib/main.dart
@@ -71,29 +71,39 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Stack(fit: StackFit.expand, alignment: Alignment.center, children: [
-        Positioned(
-          top: 5,
-          left: 0,
-          child: Image.network(
-            "https://prod-ne-cdn-media.puregym.com/media/819394/gym-workout-plan-for-gaining-muscle_header.jpg?quality=80",
-            height: 200,
-          ),
-        ),
-        ConstrainedBox(
-            constraints: const BoxConstraints(
-              maxHeight: 10,
-            ),
-            child: Stack(children: [
-              Text("Best Quarantine Workout",
-                  style: TextStyle(
-                      fontSize: 30, color: Color.fromARGB(255, 255, 255, 255))),
-              const Text(
-                "See more >",
-                style: TextStyle(
-                    fontSize: 17, color: Color.fromARGB(255, 255, 223, 0)),
+      body: Column(children: [
+        Stack(
+          children: [
+            Container(
+              margin: EdgeInsets.all(10),
+              padding: EdgeInsets.all(10),
+              child: Image.network(
+                "https://prod-ne-cdn-media.puregym.com/media/819394/gym-workout-plan-for-gaining-muscle_header.jpg?quality=80",
+                height: 200,
               ),
-            ]))
+              decoration:
+                  BoxDecoration(borderRadius: BorderRadius.circular(20)),
+              clipBehavior: Clip.hardEdge,
+            ),
+            Positioned(
+                child: Column(
+                  children: [
+                    Text("Best Quarantine Workout",
+                        style: TextStyle(
+                          fontSize: 27,
+                        )),
+                    const Text(
+                      "See more >",
+                      style: TextStyle(
+                          fontSize: 17,
+                          color: Color.fromARGB(255, 255, 223, 0)),
+                    ),
+                  ],
+                ),
+                left: 30,
+                top: 70)
+          ],
+        ),
       ]),
     );
   }
